83e29fdd127054d133706c5092ea589a9e2f3425
Andrew Leonard <andy@hurricane-ridge.com>
2013-11-05 06:56:42 -0800

    Loosen rsync command match.

--- files/validate-rsync.sh
+++ files/validate-rsync.sh
@@ -10,6 +10,6 @@ case "$SSH_ORIGINAL_COMMAND" in
     *\<*) echo "Rejected" ;;
     *\`*) echo "Rejected" ;;
     *\|*) echo "Rejected" ;;
-    rsync\ --server\ -vlogDtprze.iLsf\ .\ /*) $SSH_ORIGINAL_COMMAND ;;
+    rsync\ --server*) $SSH_ORIGINAL_COMMAND ;;
     *) echo "Rejected" ;;
 esac 
